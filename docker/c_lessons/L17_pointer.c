de53457c3e73f77ce4a4238b9a5e3c27fbd706d0
celebrater <tkgm45nbchk@gmail.com>
2022-08-22 05:55:15 +0900

    L17

---
+++ docker/c_lessons/L17_pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+
+ポインタ: アドレスを格納するための変数
+
+メモリ（記憶領域）
+
+& : アドレス演算子
+
+* : 間接演算子
+
+*/
+
+int main(void) {
+
+    int a;
+    a = 10;
+
+    int *pa;
+    pa = &a;
+
+    printf("%d\n", *pa);
+
+    return 0;
+}
